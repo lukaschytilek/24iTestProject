ea44ed5f38aeb069a7c4043eea1d0219543932e2
Lukas Chytilek <chytilek@elkoep.cz>
2020-05-03 19:37:48 +0200

    podfile

---
+++ MovieDatabase24i/Pods/Target Support Files/Pods-MovieDatabase24i-MovieDatabase24iUITests/Pods-MovieDatabase24i-MovieDatabase24iUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieDatabase24i_MovieDatabase24iUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MovieDatabase24i_MovieDatabase24iUITests
+@end
